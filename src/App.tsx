7bd95ef362907ee23b6b422103e520fcae6bdfde
Oleksandr <oleksandr.nediev@student.howest.be>
2025-02-21 16:07:08 +0100

    Add pulsing button animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 import {motion} from "framer-motion";
+import {PulsingButton} from "./components/PulsingButton";
 
 function App() {
 
   return (
     <div>
-      <motion.div className="box"
-                  animate={{ skewY: 20}}
-      />
+      {/*<motion.div className="box"
+                  initial={{ x: 0 }}
+                  animate={{x: 100}}
+                  transition={{delay: 2}}
+                  transition={{duration: 2, ease: "easeInOut"}}
+      />*/}
+
+        {/*<motion.div className="box"
+                    animate={{
+                        scale: [1, 2, 3, 2, 1],
+                        rotate: [0, 180, 90, 270]
+                    }}
+                    transition={{duration: 5}}
+        />*/}
+
+        <PulsingButton/>
     </div>
   )
 }
